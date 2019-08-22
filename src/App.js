b75c9a8a8c7fc105cbf9c9265eae94abce2048a4
Marcela <pachelina22@gmail.com>
2019-08-22 20:49:16 -0300

    seguimos mañana

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import './App.css';
 
+/* function Helloword(props) {
+  return(
+    <div id= "hello">
+      <h3>{props.subtitulo} </h3>
+    {props.mytext}
+     </div>
+  )
+} */
+/* const App = () => <div>Este es mi component: <Helloword/> </div> */
+
+class Helloword extends React.Component {
+  render () {
+   return (
+    <div id= "hello">
+    <h3>{this.props.subtitulo} </h3>
+    {this.props.mytext}
+   </div>
+   )
+  }
+} 
+
 function App() {
   return (
-    <div>Hola Mundo</div>
+    <div>Este es el componente: 
+      <Helloword mytext="Hola Pache" subtitulo=" Hola bien"/> 
+      <Helloword mytext="hola!!" subtitulo=" que tal"/> 
+      <Helloword mytext="Como estas?" subtitulo=" Todo bien"/> 
+      </div>
   );
 }
 
